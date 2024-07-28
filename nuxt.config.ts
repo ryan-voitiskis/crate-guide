510605915efe65afa09ef3f5f532e8b35fc3803e
ryan voitiskis <ryanvoitiskis@pm.me>
2024-07-28 14:56:37 +1000

    feat: install @nuxtjs/supabase and configure

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt"]
-})
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: false },
+  modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt", "@nuxtjs/supabase"],
+  supabase: {
+    url: process.env.SUPABASE_URL,
+    key: process.env.SUPABASE_KEY,
+    redirectOptions: {
+      login: "/login",
+      callback: "/confirm",
+      include: undefined,
+      exclude: [],
+      cookieRedirect: false,
+    },
+  },
+});
