7665c0c1fdc8570c4f9352699f3d540740e9e73f
ryan voitiskis <ryanvoitiskis@pm.me>
2024-07-28 18:02:05 +1000

    feat(supa): create records table migration

---
+++ supabase/migrations/20240728061347_create_records_table.sql
@@ -0,0 +1,22 @@
+create table public.records (
+  id uuid not null primary key,
+  user_id uuid not null references auth.users(id) on delete cascade,
+  discogs_id integer,
+  spotify_id varchar,
+  catno varchar,
+  title varchar not null,
+  artists varchar not null,
+  label varchar,
+  year integer,
+  cover varchar,
+  created_at timestamp with time zone default now(),
+  updated_at timestamp with time zone default now()
+);
+
+alter table public.records enable row level security;
+
+create policy "Users can only access their own records"
+  on public.records for all
+  using (auth.uid() = user_id);
+
+create index idx_records_user_id on public.records (user_id);
