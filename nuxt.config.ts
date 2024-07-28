8c26c243f388d41905e2431a68c0ea8af8a3f94c
ryan voitiskis <ryanvoitiskis@pm.me>
2024-07-28 15:25:44 +1000

    build: install prettier + run format

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,22 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2024-04-03",
-  devtools: { enabled: false },
-  modules: [
-    "@nuxt/eslint",
-    "@nuxtjs/supabase",
-    "@nuxtjs/tailwindcss",
-    "shadcn-nuxt",
-  ],
-  supabase: {
-    url: process.env.SUPABASE_URL,
-    key: process.env.SUPABASE_KEY,
-    redirectOptions: {
-      login: "/login",
-      callback: "/confirm",
-      include: undefined,
-      exclude: [],
-      cookieRedirect: false,
-    },
-  },
-});
+	compatibilityDate: '2024-04-03',
+	devtools: { enabled: false },
+	modules: [
+		'@nuxt/eslint',
+		'@nuxtjs/supabase',
+		'@nuxtjs/tailwindcss',
+		'shadcn-nuxt'
+	],
+	supabase: {
+		url: process.env.SUPABASE_URL,
+		key: process.env.SUPABASE_KEY,
+		redirectOptions: {
+			login: '/login',
+			callback: '/confirm',
+			include: undefined,
+			exclude: [],
+			cookieRedirect: false
+		}
+	}
+})
