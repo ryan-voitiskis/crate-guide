aebe7eef2229e0521c1713caae822fe8c83a7b81
ryan voitiskis <ryanvoitiskis@protonmail.com>
2025-08-24 22:46:00 +1000

    refactor(ui): modernise shadcn-vue Separator component

--- app/components/ui/separator/Separator.vue
+++ app/components/ui/separator/Separator.vue
@@ -1,45 +1,29 @@
 <script setup lang="ts">
-import { type HTMLAttributes, computed } from 'vue'
-import { Separator, type SeparatorProps } from 'radix-vue'
+import type { HTMLAttributes } from 'vue'
+import { reactiveOmit } from '@vueuse/core'
+import type { SeparatorProps } from 'reka-ui'
+import { Separator } from 'reka-ui'
 
-const props = defineProps<
-	SeparatorProps & {
-		class?: HTMLAttributes['class']
-		label?: string
-		spanClass?: HTMLAttributes['class']
+const props = withDefaults(
+	defineProps<SeparatorProps & { class?: HTMLAttributes['class'] }>(),
+	{
+		orientation: 'horizontal',
+		decorative: true
 	}
->()
+)
 
-const delegatedProps = computed(() => {
-	const { class: _, spanClass: __, ...delegated } = props
-	return delegated
-})
+const delegatedProps = reactiveOmit(props, 'class')
 </script>
 
 <template>
 	<Separator
+		data-slot="separator-root"
 		v-bind="delegatedProps"
 		:class="
 			cn(
-				'bg-border relative shrink-0',
-				props.orientation === 'vertical' ? 'h-full w-px' : 'h-px w-full',
+				`bg-border shrink-0 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px`,
 				props.class
 			)
 		"
-	>
-		<span
-			v-if="props.label"
-			:class="
-				cn(
-					'bg-background text-muted-foreground absolute top-1/2 left-1/2 flex -translate-x-1/2 -translate-y-1/2 items-center justify-center text-xs',
-					props.orientation === 'vertical'
-						? 'w-px px-1 py-2'
-						: 'h-px px-2 py-1',
-					props.spanClass
-				)
-			"
-		>
-			{{ props.label }}
-		</span>
-	</Separator>
+	/>
 </template>
