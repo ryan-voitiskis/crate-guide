89bd7b63f350130bf91c2d380b1b75f93882cddc
ryan voitiskis <ryanvoitiskis@protonmail.com>
2025-08-24 22:42:02 +1000

    refactor(ui): modernise shadcn-vue RadioGroup component

--- app/components/ui/radio-group/RadioGroupItem.vue
+++ app/components/ui/radio-group/RadioGroupItem.vue
@@ -1,38 +1,37 @@
 <script setup lang="ts">
-import { type HTMLAttributes, computed } from 'vue'
-import { Circle } from 'lucide-vue-next'
-import {
-	RadioGroupIndicator,
-	RadioGroupItem,
-	type RadioGroupItemProps,
-	useForwardProps
-} from 'radix-vue'
+import type { HTMLAttributes } from 'vue'
+import { reactiveOmit } from '@vueuse/core'
+import { CircleIcon } from 'lucide-vue-next'
+import type { RadioGroupItemProps } from 'reka-ui'
+import { RadioGroupIndicator, RadioGroupItem, useForwardProps } from 'reka-ui'
 
 const props = defineProps<
 	RadioGroupItemProps & { class?: HTMLAttributes['class'] }
 >()
 
-const delegatedProps = computed(() => {
-	const { class: _, ...delegated } = props
-
-	return delegated
-})
+const delegatedProps = reactiveOmit(props, 'class')
 
 const forwardedProps = useForwardProps(delegatedProps)
 </script>
 
 <template>
 	<RadioGroupItem
+		data-slot="radio-group-item"
 		v-bind="forwardedProps"
 		:class="
 			cn(
-				'border-primary text-primary ring-offset-background focus-visible:ring-ring aspect-square h-4 w-4 rounded-full border focus:outline-hidden focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+				'border-input text-primary focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-input/30 aspect-square size-4 shrink-0 rounded-full border shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50',
 				props.class
 			)
 		"
 	>
-		<RadioGroupIndicator class="flex items-center justify-center">
-			<Circle class="h-2.5 w-2.5 fill-current text-current" />
+		<RadioGroupIndicator
+			data-slot="radio-group-indicator"
+			class="relative flex items-center justify-center"
+		>
+			<CircleIcon
+				class="fill-primary absolute top-1/2 left-1/2 size-2 -translate-x-1/2 -translate-y-1/2"
+			/>
 		</RadioGroupIndicator>
 	</RadioGroupItem>
 </template>
