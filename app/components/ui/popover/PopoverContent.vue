91ba756c219c9168e5061e3a5f3f4589f5f47982
ryan voitiskis <ryanvoitiskis@protonmail.com>
2025-08-24 22:40:03 +1000

    refactor(ui): modernise shadcn-vue Popover component

--- app/components/ui/popover/PopoverContent.vue
+++ app/components/ui/popover/PopoverContent.vue
@@ -1,12 +1,8 @@
 <script setup lang="ts">
-import { type HTMLAttributes, computed } from 'vue'
-import {
-	PopoverContent,
-	type PopoverContentEmits,
-	type PopoverContentProps,
-	PopoverPortal,
-	useForwardPropsEmits
-} from 'radix-vue'
+import type { HTMLAttributes } from 'vue'
+import { reactiveOmit } from '@vueuse/core'
+import type { PopoverContentEmits, PopoverContentProps } from 'reka-ui'
+import { PopoverContent, PopoverPortal, useForwardPropsEmits } from 'reka-ui'
 
 defineOptions({
 	inheritAttrs: false
@@ -21,11 +17,7 @@ const props = withDefaults(
 )
 const emits = defineEmits<PopoverContentEmits>()
 
-const delegatedProps = computed(() => {
-	const { class: _, ...delegated } = props
-
-	return delegated
-})
+const delegatedProps = reactiveOmit(props, 'class')
 
 const forwarded = useForwardPropsEmits(delegatedProps, emits)
 </script>
@@ -33,10 +25,11 @@ const forwarded = useForwardPropsEmits(delegatedProps, emits)
 <template>
 	<PopoverPortal>
 		<PopoverContent
+			data-slot="popover-content"
 			v-bind="{ ...forwarded, ...$attrs }"
 			:class="
 				cn(
-					'border-border bg-popover text-popover-foreground data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 w-72 rounded-md border p-4 shadow-md outline-hidden',
+					'bg-popover text-popover-foreground data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 w-72 origin-(--reka-popover-content-transform-origin) rounded-md border p-4 shadow-md outline-hidden',
 					props.class
 				)
 			"
