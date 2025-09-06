09bc48e799b93d28f8c67db53aba98447753501f
ryan voitiskis <ryanvoitiskis@protonmail.com>
2025-09-07 03:02:17 +1000

    feat: add shadcn-vue Table + @tabstack/vue-table

---
+++ app/components/ui/table/TableEmpty.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from 'vue'
+import { reactiveOmit } from '@vueuse/core'
+import TableCell from './TableCell.vue'
+import TableRow from './TableRow.vue'
+
+const props = withDefaults(
+	defineProps<{
+		class?: HTMLAttributes['class']
+		colspan?: number
+	}>(),
+	{
+		colspan: 1
+	}
+)
+
+const delegatedProps = reactiveOmit(props, 'class')
+</script>
+
+<template>
+	<TableRow>
+		<TableCell
+			:class="
+				cn(
+					'text-foreground p-4 align-middle text-sm whitespace-nowrap',
+					props.class
+				)
+			"
+			v-bind="delegatedProps"
+		>
+			<div class="flex items-center justify-center py-10">
+				<slot />
+			</div>
+		</TableCell>
+	</TableRow>
+</template>
