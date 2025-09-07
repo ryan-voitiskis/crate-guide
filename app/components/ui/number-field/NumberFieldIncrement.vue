f75882984de4e603d0e10ff76022da74ed9dfd33
ryan voitiskis <ryanvoitiskis@protonmail.com>
2025-09-07 22:58:52 +1000

    feat: add shadcn-vue NumberField UI component

---
+++ app/components/ui/number-field/NumberFieldIncrement.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from 'vue'
+import { reactiveOmit } from '@vueuse/core'
+import { Plus } from 'lucide-vue-next'
+import type { NumberFieldIncrementProps } from 'reka-ui'
+import { NumberFieldIncrement, useForwardProps } from 'reka-ui'
+
+const props = defineProps<
+	NumberFieldIncrementProps & { class?: HTMLAttributes['class'] }
+>()
+
+const delegatedProps = reactiveOmit(props, 'class')
+
+const forwarded = useForwardProps(delegatedProps)
+</script>
+
+<template>
+	<NumberFieldIncrement
+		data-slot="increment"
+		v-bind="forwarded"
+		:class="
+			cn(
+				'absolute top-1/2 right-0 -translate-y-1/2 p-3 disabled:cursor-not-allowed disabled:opacity-20',
+				props.class
+			)
+		"
+	>
+		<slot>
+			<Plus class="h-4 w-4" />
+		</slot>
+	</NumberFieldIncrement>
+</template>
